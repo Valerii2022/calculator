40842ba1de9688467ae3b39852f757733019077d
Valerii Pometun <vmpometun@gmail.com>
2023-12-22 11:08:57 +0200

    update markup/added some logic

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,31 @@
-// let expression: string = "";
-// let result: number = 0;
-// let operator: string = "";
+const buttonsRef = document.querySelector(".btn-wrapper");
 
-// const refs: any = {
-//   wrapper: document.querySelector(".btn-wrapper"),
-//   display: document.querySelector(".display"),
-// };
+buttonsRef?.addEventListener("click", appendToResult);
 
-// refs.wrapper.addEventListener("click", calculate);
+function appendToResult(e) {
+  if (e.target.id === "clear") {
+    clearResult();
+    return;
+  }
+  if (e.target.id === "equal") {
+    calculateResult();
+    return;
+  }
+  document.getElementById("result").value += e.target.id;
+}
 
-// function calculate(e) {
-//   if (Number(e.target.textContent)) {
-//     expression += e.target.textContent;
-//     refs.display.value = expression;
-//   }
-//   if (e.target.textContent === "+") {
-//     operator === "+";
-//     result = Number(expression);
-//     expression = "";
-//   }
-//   if (e.target.textContent === "-") {
-//     operator === "-";
-//     result = Number(expression);
-//     expression = "";
-//   }
-//   if (e.target.textContent === "=") {
-//     if (operator === "+") {
-//       result += Number(expression);
-//     }
-//     if (operator === "-") {
-//       result -= Number(expression);
-//     }
-//     refs.display.value = result;
-//     expression = "";
-//   }
-//   if (e.target.textContent === "C") {
-//     expression = "";
-//     result = 0;
-//     operator = "";
-//     refs.display.value = "";
-//   }
-// }
+function clearResult() {
+  document.getElementById("result").value = "";
+}
+
+function calculateResult() {
+  const result = document.getElementById("result").value;
+  try {
+    const sanitizedResult = result.replace(/[^-()\d/*+.]/g, "");
+    document.getElementById("result").value = Function(
+      '"use strict";return (' + sanitizedResult + ")"
+    )();
+  } catch (error) {
+    document.getElementById("result").value = "Error";
+  }
+}
